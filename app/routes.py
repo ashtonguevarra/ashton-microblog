40cc607c48e506b18435cf4e08ff18de355b1ff6
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-03-01 14:24:17 +0800

    add index template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "I love Tonee!"
+    user = {'username': 'Ashton'}
+    posts = [
+        {
+            'author': {'username': 'Dom'},
+            'body': 'I pissed my pants today.'
+        },
+        {
+            'author': {'username': 'Tonee'},
+            'body': 'I think I have a crush on Ashton.'
+        },
+        {
+            'author': {'username': 'Pau'},
+            'body': 'so uhh we broke up...'
+        },
+        {
+            'author': {'username': 'Drei'},
+            'body': 'best night ever with robie!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
